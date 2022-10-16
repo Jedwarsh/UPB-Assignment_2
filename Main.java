b7cbee32c2cca7df0fc7ae7d86066b5613d1e9aa
Jedwarsh <110847381+Jedwarsh@users.noreply.github.com>
2022-10-16 18:54:17 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -13,6 +13,30 @@ import java.util.Random;
 
 public class Main {
 
+    public static void main(String[] args) throws IOException, InvalidKeySpecException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
+        if (args.length == 3 && args[0].equals("D")) {
+            File encrypted = new File(args[2]);
+            File decrypted = new File("decrypted");
+            BufferedReader bf = new BufferedReader(new FileReader(args[1]));
+            String keyLine = bf.readLine();
+            encryptDecrypt(keyLine, Cipher.DECRYPT_MODE, encrypted, decrypted);
+        } else if (args.length == 2 && args[0].equals("E")) {
+            System.out.println(System.getProperty("user.dir"));
+            File plaintext = new File(args[1]);
+            File encrypted = new File("encrypted");
+            String key = createRandomKey();
+            encryptDecrypt(key, Cipher.ENCRYPT_MODE, plaintext, encrypted);
+        } else {
+            System.out.println("Wrong number or usage of arguments.");
+        }
+    }
+
+    private  static String createRandomKey(){
+        Random rnd = new Random();
+        int n = 10000000 + rnd.nextInt(90000000);
+        return String.valueOf(n);
+    }
+
     public static void encryptDecrypt(String key, int cipherMode, File in, File output) throws IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException {
         FileInputStream fis = new FileInputStream(in);
         FileOutputStream fos = new FileOutputStream(output);
@@ -31,15 +55,15 @@ public class Main {
             long start = System.nanoTime();
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv, SecureRandom.getInstance("SHA1PRNG"));
             CipherInputStream cis = new CipherInputStream(fis, cipher);
-            write(cis,fos);
-            PrintWriter out = new PrintWriter("KeyFromFile");
+            write(cis, fos);
+            PrintWriter out = new PrintWriter("key");
             out.println(key);
             out.close();
             long end = System.nanoTime();
             long duration = end - start;
             double durationInSeconds = (double) duration/ 1_000_000_000;
             System.out.println("Encryption took " + durationInSeconds + " seconds");
-        }else if(cipherMode == Cipher.DECRYPT_MODE) {
+        } else if(cipherMode == Cipher.DECRYPT_MODE) {
             long start = System.nanoTime();
             cipher.init(Cipher.DECRYPT_MODE, secretKey, iv, SecureRandom.getInstance("SHA1PRNG"));
             CipherOutputStream cos = new CipherOutputStream(fos, cipher);
@@ -52,33 +76,6 @@ public class Main {
     }
 
     private static void write(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[64];
-        int numOfBytesRead;
-        while((numOfBytesRead = in.read(buffer)) != -1){
-            out.write(buffer, 0, numOfBytesRead);
-        }
-        in.close();
-        out.close();
-    }
-    private  static String createRandomKey(){
-        Random rnd = new Random();
-        int n = 10000000 + rnd.nextInt(90000000);
-        return String.valueOf(n);
-    }
-
-    public static void main(String[] args) throws IOException, InvalidKeySpecException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
-        if (args.length == 3 && args[0].equals("D")) {
-            File encrypted = new File(args[2]);
-            File decrypted = new File("decrypted");
-            BufferedReader bf = new BufferedReader(new FileReader(args[1]));
-            String keyLine = bf.readLine();
-            encryptDecrypt(keyLine, Cipher.DECRYPT_MODE, encrypted, decrypted);
-        } else if (args.length == 2 && args[0].equals("E")) {
-            System.out.println(System.getProperty("user.dir"));
-            File plaintext = new File(args[1]);
-            File encrypted = new File("encrypted");
-            String key = createRandomKey();
-            encryptDecrypt(key, Cipher.ENCRYPT_MODE, plaintext, encrypted);
-        }
+        in.transferTo(out);
     }
 }
